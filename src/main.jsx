917a9873901d79405ceec8c6c139d472b11e2533
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 15:02:35 -0500

    Mount scenario engine panel

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import AcademyProgress from './AcademyProgress.jsx';
+import ScenarioEnginePanel from './ScenarioEnginePanel.jsx';
 import './styles.css';
 import './records.css';
 import './case-summary.css';
 import './lunaDebrief.css';
 import './academyProgress.css';
+import './scenarioEngine.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ScenarioEnginePanel />
     <AcademyProgress />
   </React.StrictMode>
 );
