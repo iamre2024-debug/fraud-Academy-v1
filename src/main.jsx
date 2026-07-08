87a6bda61ef9d6af2ac60a7a0b88c77b65062c07
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 13:12:59 -0500

    Import case summary styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './records.css';
+import './case-summary.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
