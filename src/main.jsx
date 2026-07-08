deed85c51e25ce3d3bdff59fed4193366902a6f4
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 18:25:03 -0500

    Load visual polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import VisualWorkspace from './VisualWorkspace.jsx';
 import './visualWorkspace.css';
+import './visualPolish.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
