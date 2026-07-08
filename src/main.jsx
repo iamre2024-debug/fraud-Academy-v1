919e87211dc75b41752389832178a8607d76598e
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 14:15:40 -0500

    Mount academy progress layer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import AcademyProgress from './AcademyProgress.jsx';
 import './styles.css';
 import './records.css';
 import './case-summary.css';
 import './lunaDebrief.css';
+import './academyProgress.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AcademyProgress />
   </React.StrictMode>
 );
