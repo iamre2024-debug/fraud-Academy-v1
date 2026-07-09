c8db5c5207a9f732df5c2fd2aba34dfd84f174e1
iamre2024-debug <iamree2024@gmail.com>
2026-07-09 17:00:46 -0500

    Render React-managed compact text controls

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import VisualWorkspace from './VisualWorkspace.jsx';
 import VisualNavigation from './VisualNavigation.jsx';
+import VisualTextCollapse from './VisualTextCollapse.jsx';
 import './visualWorkspace.css';
 import './visualPolish.css';
 import './mobileViewportFix.css';
@@ -11,11 +12,11 @@ import './visualDesktopCommand.css';
 import './visualNavPatch.css';
 import './visualTextCollapse.css';
 import './visualQaPatch.js';
-import './visualTextCollapse.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <VisualWorkspace />
     <VisualNavigation />
+    <VisualTextCollapse />
   </React.StrictMode>,
 );
