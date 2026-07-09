b1c98b2df5dafec0f83c319334e1ed10e8d5b014
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 22:19:53 -0500

    Load visual functional controls

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import VisualWorkspace from './VisualWorkspace.jsx';
 import './visualWorkspace.css';
 import './visualPolish.css';
 import './mobileViewportFix.css';
+import './visualFunctional.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
