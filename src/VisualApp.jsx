56393404bf86c22cde13f92e6f97be4298df6868
iamre2024-debug <iamree2024@gmail.com>
2026-07-09 19:03:57 -0500

    Render Luna post submission module

--- src/VisualApp.jsx
+++ src/VisualApp.jsx
@@ -3,6 +3,7 @@ import VisualWorkspace from './VisualWorkspace.jsx';
 import VisualNavigation from './VisualNavigation.jsx';
 import VisualTextCollapse from './VisualTextCollapse.jsx';
 import SystemAccessLane from './SystemAccessLane.jsx';
+import LunaPostSubmissionPanel from './LunaPostSubmissionPanel.jsx';
 import { trainingCases } from './data/cases.js';
 
 export default function VisualApp() {
@@ -26,6 +27,7 @@ export default function VisualApp() {
         onNavigate={setActiveTab}
       />
       <SystemAccessLane activeCaseId={activeCaseId} />
+      <LunaPostSubmissionPanel activeCaseId={activeCaseId} />
       <VisualNavigation
         activeTab={activeTab}
         onNavigate={setActiveTab}
