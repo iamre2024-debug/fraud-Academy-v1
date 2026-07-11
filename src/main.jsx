ad965bf36529dfba0f8e99d73d68258c1d491da5
iamre2024-debug <iamree2024@gmail.com>
2026-07-10 19:43:59 -0500

    Wire Dashboard and Cases panel styles

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './systemAccessLane.css';
 import './generatedCaseControls.css';
 import './browserInteractionStability.css';
 import './lightMoonlightLace.css';
+import './displayNavigationPanels.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
