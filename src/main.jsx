ab18cb15a2a38831e394e7f3fecf14b4b0a38f58
iamre2024-debug <iamree2024@gmail.com>
2026-07-13 12:30:12 -0500

    Load Customer 360 attached page styles

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ import './displayProfileThemeV1.css';
 import './displayProfileThemeV1Safety.css';
 import './displayFinalResponsivePolishV1.css';
 import './mobileVisualCorrectnessRepair.css';
-import './caseReportRemoval.css';
+import './customer360AttachedPages.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
