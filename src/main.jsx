95096e7011605d9cbf24de92489a6153292acf03
iamre2024-debug <iamree2024@gmail.com>
2026-07-19 21:47:45 -0500

    Load Luna manager mobile overrides

--- src/main.jsx
+++ src/main.jsx
@@ -35,6 +35,7 @@ import './displayProfileThemeV1Safety.css';
 import './displayFinalResponsivePolishV1.css';
 import './responsiveLayoutMode.css';
 import './mobileAppPages.css';
+import './lunaManagerMobileFix.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
